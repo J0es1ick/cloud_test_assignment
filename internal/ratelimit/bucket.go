1315e9ded5c95f716b90c04e584ffff0497bed3d
J0es1ick <mrjoeslick@gmail.com>
2025-05-14 10:21:27 +0300

    added token bucket

---
+++ internal/ratelimit/bucket.go
@@ -0,0 +1,45 @@
+package ratelimit
+
+import (
+	"time"
+)
+
+type TokenBucket struct {
+	capacity   int
+	tokens     int
+	rate       time.Duration
+	lastRefill time.Time
+}
+
+func NewTokenBucket(capacity int, rate time.Duration) *TokenBucket {
+	return &TokenBucket{
+		capacity:   capacity,
+		tokens:     capacity,
+		rate:       rate,
+		lastRefill: time.Now(),
+	}
+}
+
+func (tb *TokenBucket) refill() {
+	now := time.Now()
+	elapsed := now.Sub(tb.lastRefill)
+	tb.lastRefill = now
+
+	newTokens := int(elapsed / tb.rate)
+	if newTokens > 0 {
+		tb.tokens += newTokens
+		if tb.tokens > tb.capacity {
+			tb.tokens = tb.capacity
+		}
+	}
+}
+
+func (tb *TokenBucket) Take() bool {
+	tb.refill()
+	
+	if tb.tokens > 0 {
+		tb.tokens--
+		return true
+	}
+	return false
+}
